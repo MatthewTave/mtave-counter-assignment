42b59ddc8bd895a066531c0d1e938e149f7515fc
Matthew Tave <tavematthew@gmail.com>
2024-04-03 18:54:24 -0400

    Added unit tests for counter component

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,43 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, fireEvent, screen } from '@testing-library/react';
+import Counter from '../components/Counter';
 
+// Test Constants
+const COUNT_ELEMENT_TESTID = 'count';
+const COUNT_LABEL_TEXT = /count/i;
+const TEST_RANGE = Math.pow(2, 15);
+
+// Elements that are being tested
+let countElement, incrementButton, decrementButton;
+
+// Renders element to be tested before each test
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />);
+
+  countElement = screen.getByTestId(COUNT_ELEMENT_TESTID);
+  incrementButton = screen.getByText('+');
+  decrementButton = screen.getByText('-');
 })
 
+// Test Suite
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const countLabelElement = screen.getByText(COUNT_LABEL_TEXT);
+  expect(countLabelElement).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  expect(countElement.textContent).toBe('0');
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  for(let i = 0; i <= TEST_RANGE; i++) {
+    expect(countElement.textContent).toBe(i.toString());
+    fireEvent.click(incrementButton);
+  }
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  for(let i = 0; i >= TEST_RANGE * -1; i--) {
+    expect(countElement.textContent).toBe(i.toString());
+    fireEvent.click(decrementButton);
+  }
 });
